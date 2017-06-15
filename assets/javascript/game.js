bc1fcb4aef958cf2cf26e3c4ded61d679715d093
Maerk Q <wj_hilliard@yahoo.com>
2017-06-15 14:37:24 -0400

    Boxes represented, style needed, win/loss needs fix

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// for (var i = 0; i < numberOptions.length; i++) {
+  //  var imageOrb = $("<img>");//change to imageOrb
+    //imageOrb.addClass("orb-space");
+    
+    // Each imageOrb will be given a data attribute called data-orbvalue.
+    // This data attribute will be set equal to the array value.
+   // imageOrb.attr("data-orbvalue", numberOptions[i]);
+
+    // Lastly, each orb image (with all it classes and attributes) will get added to the page.
+   // $("#orbs").append(imageOrb);
+  //}
+   //$(".orb-space").on("click", function() {//sets .orb-space as reacting to click function
+    //var orbValue = ($(this).attr("data-orbvalue"));//value taken from "data-orbvalue" attribute
+    //orbValue = parseInt(orbValue);//convert html string to integer
+    //userTotal += orbValue;//per click, userTotal increases by an orb's value
+    //alert("You're score is: " + userTotal);
